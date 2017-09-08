84df2b946a669e61ad29625665200088b235b7e0
drgauvin <drgauvin@mgmcare.org>
2017-09-08 13:06:33 -0400

    build

---
+++ webpack.prod.js
@@ -0,0 +1,47 @@
+const webpack = require('webpack');
+const path = require('path');
+
+
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, 'public')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ["es2015", "react", "stage-0"]
+        }
+      }
+    ]
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: false,
+      screw_ie8: true,
+      conditionals: true,
+      unused: true,
+      comparisons: true,
+      sequences: true,
+      dead_code: true,
+      evaluate: true,
+      if_return: true,
+      join_vars: true
+    },
+    output: {
+      comments: false
+    }
+  }),
+  new webpack.DefinePlugin({
+   'process.env.NODE_ENV': JSON.stringify('production')
+ })
+
+  ]
+}
